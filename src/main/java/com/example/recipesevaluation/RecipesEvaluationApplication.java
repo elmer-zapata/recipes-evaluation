7387abaaa255da83db9c96cef02dc570d38dc652
elmer-zapata-tierconnect-com <elmer.zapata@tierconnect.com>
2019-01-07 11:40:08 -0400

    Structure

---
+++ src/main/java/com/example/recipesevaluation/RecipesEvaluationApplication.java
@@ -0,0 +1,14 @@
+package com.example.recipesevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesEvaluationApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/recipesevaluation/RecipesEvaluationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.recipesevaluation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipesEvaluationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
